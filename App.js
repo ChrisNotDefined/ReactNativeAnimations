47ffcafbe12931ff39f71be48e5b1aa101cc4e32
ChrisNotDefined <christopherapstarwars3@gmail.com>
2021-04-13 13:20:02 -0500

    Appliing animations to gestures

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Animated } from "react-native";
+import { StyleSheet, Text, View, Animated, PanResponder } from "react-native";
 
 export default function App() {
   const position = new Animated.ValueXY({ x: 0, y: 0 });
 
-  Animated.timing(position, {
-    toValue: { x: 200, y: 500 },
-    duration: 2000,
-    useNativeDriver: true,
-  }).start();
+  // Animated.timing(position, {
+  //   toValue: { x: 200, y: 500 },
+  //   duration: 2000,
+  //   useNativeDriver: true,
+  // }).start();
+
+  const pan = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+
+    // onPanResponderMove: Animated.event(
+    //   [null, { dx: position.x, dy: position.y }],
+    //   { useNativeDriver: false }
+    // ),
+
+    // Same thing
+    onPanResponderMove: (e, gesture) => {
+      position.setValue({x: gesture.dx, y: gesture.dy})
+    },
+
+    onPanResponderRelease: () => {
+      //position.setValue({ x: 0, y: 0 });
+      Animated.spring(position, {
+        toValue: { x: 0, y: 0 },
+        bounciness: 20,
+        useNativeDriver: false,
+      }).start();
+    },
+  });
 
   const rotate = position.x.interpolate({
     inputRange: [0, 100],
@@ -19,6 +42,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Animated.View
+        {...pan.panHandlers}
         style={[
           styles.textView,
           {
@@ -30,7 +54,7 @@ export default function App() {
           },
         ]}
       >
-        <Text>Dragon</Text>
+        <Text>Jelly</Text>
       </Animated.View>
     </View>
   );
@@ -39,6 +63,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   textView: {
     height: 80,
